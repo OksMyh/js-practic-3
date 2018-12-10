df1d6d6f9d37d9d7ce7c54be7e9111cb21dc7bdb
Oksana <oks.mykhailiuk@gmail.com>
2018-12-10 08:27:53 +0200

    practic dom task 3

---
+++ practic-3-3.js
@@ -0,0 +1,20 @@
+// - Створити функцію, яка генерує таблицю, перший аргумент визначає кількість строк,
+//     а другий параметр визначає кліькіть ячеєк в кожній строці.
+//     третій параметр визначає елемент в який потрібно таблицю додати
+
+function ganereteTable(row,cell,callback) {
+    let table = document.createElement('table');
+    callback('p').appendChild(table,null);
+    for (let i=0;i<row;i++){
+        let newRow = table.insertRow();
+        for (let j=0;j<cell;j++){
+            newRow.insertCell();
+        }
+    }
+
+}
+ganereteTable(5,8,function (element) {
+    let p = document.createElement(element);
+   return document.body.appendChild(p);
+
+});
