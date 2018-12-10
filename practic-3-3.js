124ff61c7705b4871a0541d4b1371c7e60bb1ef0
Oksana <oks.mykhailiuk@gmail.com>
2018-12-10 09:13:48 +0200

    practic dom task 3(method-2)

--- practic-3-3.js
+++ practic-3-3.js
@@ -2,9 +2,28 @@
 //     а другий параметр визначає кліькіть ячеєк в кожній строці.
 //     третій параметр визначає елемент в який потрібно таблицю додати
 
-function ganereteTable(row,cell,callback) {
+// function ganereteTable(row,cell,callback) {
+//     let table = document.createElement('table');
+//     callback('p').appendChild(table);
+//     for (let i=0;i<row;i++){
+//         let newRow = table.insertRow();
+//         for (let j=0;j<cell;j++){
+//             newRow.insertCell();
+//         }
+//     }
+//
+// }
+// ganereteTable(5,8,function (element) {
+//     let p = document.createElement(element);
+//    return document.body.appendChild(p);
+//
+// });
+
+// second method
+
+function generateTable(row,cell,element) {
     let table = document.createElement('table');
-    callback('p').appendChild(table,null);
+    element.appendChild(table);
     for (let i=0;i<row;i++){
         let newRow = table.insertRow();
         for (let j=0;j<cell;j++){
@@ -13,8 +32,4 @@ function ganereteTable(row,cell,callback) {
     }
 
 }
-ganereteTable(5,8,function (element) {
-    let p = document.createElement(element);
-   return document.body.appendChild(p);
-
-});
+generateTable(8,3,document.getElementsByTagName('div')[0]);
